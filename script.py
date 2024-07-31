c3e8affcb806d7eba2d8a7e5c86592a0639fc433
eonsparks <menemeshu@gmail.com>
2024-07-30 22:26:45 -0200

    added the linear regression model

---
+++ script.py
@@ -0,0 +1,7 @@
+# Required libraries 
+import matplotlib.pyplot as plt # For visualization
+import pandas as pd # For data manipulation
+import numpy as np # For calculations
+
+# Load dataset
+data = pd.read_csv("/Salary_Data.csv")
